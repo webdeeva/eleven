e2aa4bc00921f9246a1183a2d98b7c2ab09b1ecc
webdeeva <tavonia@gmail.com>
2023-10-28 08:58:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 const express = require('express');
-const { resolve } = require('path');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const fs = require('fs');
 
 const app = express();
-const port = 3010;
+const PORT = 3000;
 
-app.use(express.static('static'));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.post('/api/upload', async (req, res) => {
+    const { name, files, description, labels, apiKey } = req.body;
+
+    const formData = new FormData();
+    formData.append('name', name);
+    
+    // Assuming files is an array of URLs, download and append them to formData
+    for (let fileUrl of files) {
+        const response = await axios.get(fileUrl, { responseType: 'arraybuffer' });
+        const buffer = Buffer.from(response.data, 'binary');
+        formData.append('files', buffer, {
+            contentType: 'audio/mpeg',
+            filename: 'sample.mp3' // Modify filename as per requirement
+        });
+    }
+
+    formData.append('description', description);
+    formData.append('labels', JSON.stringify(labels));
+
+    try {
+        const response = await axios.post('https://api.elevenlabs.io/v1/voices/add', formData, {
+            headers: {
+                'accept': 'application/json',
+                'xi-api-key': apiKey,
+                ...formData.getHeaders()
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).send('Error uploading files to ElevenLabs API.');
+    }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });

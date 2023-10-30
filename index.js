7afb9ed4d761479193cfc337cdb628c2342392a3
webdeeva <tavonia@gmail.com>
2023-10-29 23:52:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const fs = require('fs');
 const FormData = require('form-data');
 
 const app = express();
@@ -9,38 +8,28 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 
-function logError(message) {
-    const timestamp = new Date().toISOString();
-    const formattedMessage = `${timestamp} - ${message}\n`;
-    fs.appendFileSync('error.log', formattedMessage);
-}
-
 app.post('/api/upload', async (req, res) => {
-    const { name, file, description, labels, apiKey, ElementId } = req.body;
+    const { name, files, description, labels, apiKey, ElementId } = req.body;
 
-    // Immediately respond indicating processing has started
-    res.json({ status: 'Processing started' });
-
-    // Process the audio in the background
-    processAudio(name, file, description, labels, apiKey, ElementId);
-});
-
-async function processAudio(name, file, description, labels, apiKey, ElementId) {
-    try {
-        const formData = new FormData();
-        formData.append('name', name);
+    const formData = new FormData();
+    formData.append('name', name);
     
-        const response = await axios.get(file, { responseType: 'arraybuffer' });
+    // Assuming files is an array of URLs, download and append them to formData
+    for (let fileUrl of files) {
+        const response = await axios.get(fileUrl, { responseType: 'arraybuffer' });
         const buffer = Buffer.from(response.data, 'binary');
         formData.append('files', buffer, {
-            contentType: 'audio/webm',
-            filename: 'sample.webm'
+            contentType: 'audio/mpeg',
+            filename: 'sample.mp3'
         });
+    }
 
-        formData.append('description', description);
-        formData.append('labels', JSON.stringify(labels));
+    formData.append('description', description);
+    formData.append('labels', JSON.stringify(labels));
 
-        const uploadResponse = await axios.post('https://api.elevenlabs.io/v1/voices/add', formData, {
+    try {
+        // Upload to ElevenLabs API
+        const elevenLabsResponse = await axios.post('https://api.elevenlabs.io/v1/voices/add', formData, {
             headers: {
                 'accept': 'application/json',
                 'xi-api-key': apiKey,
@@ -48,19 +37,36 @@ async function processAudio(name, file, description, labels, apiKey, ElementId) 
             }
         });
 
+        // Update Adalo API after successful upload to ElevenLabs
         await axios.put(`https://api.adalo.com/v0/apps/9576576a-696d-4132-bdf0-b51ebeae7c34/collections/t_060c36988b7b4f1b935be317a88d0e79/${ElementId}`, {
-            response: "Audio processed successfully!"
+            response: "Audio uploaded successfully!"
         }, {
             headers: {
                 'Authorization': 'Bearer 93cs2pkvgeemz9dz6haed6aqk',
                 'Content-Type': 'application/json'
             }
         });
+
+        res.json(elevenLabsResponse.data);
+
     } catch (error) {
-        const errorMessage = `Error during processAudio function: ${error}`;
-        logError(errorMessage);
+        // If there's an error, update Adalo API with the error information
+        try {
+            await axios.put(`https://api.adalo.com/v0/apps/9576576a-696d-4132-bdf0-b51ebeae7c34/collections/t_060c36988b7b4f1b935be317a88d0e79/${ElementId}`, {
+                log: `Error: ${error.message || error}`
+            }, {
+                headers: {
+                    'Authorization': 'Bearer 93cs2pkvgeemz9dz6haed6aqk',
+                    'Content-Type': 'application/json'
+                }
+            });
+        } catch (adaloError) {
+            console.error("Error updating Adalo API:", adaloError.message || adaloError);
+        }
+
+        res.status(500).send('Error processing the request.');
     }
-}
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);

36a50bb648e5b196815560a77dd63ad5c85eedda
webdeeva <tavonia@gmail.com>
2023-10-29 23:51:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const fs = require('fs');
+const FormData = require('form-data');
 
 const app = express();
 const PORT = 3000;
@@ -9,7 +9,7 @@ const PORT = 3000;
 app.use(bodyParser.json());
 
 app.post('/api/upload', async (req, res) => {
-    const { name, files, description, labels, apiKey } = req.body;
+    const { name, files, description, labels, apiKey, ElementId } = req.body;
 
     const formData = new FormData();
     formData.append('name', name);
@@ -20,7 +20,7 @@ app.post('/api/upload', async (req, res) => {
         const buffer = Buffer.from(response.data, 'binary');
         formData.append('files', buffer, {
             contentType: 'audio/mpeg',
-            filename: 'sample.mp3' // Modify filename as per requirement
+            filename: 'sample.mp3'
         });
     }
 
@@ -28,16 +28,43 @@ app.post('/api/upload', async (req, res) => {
     formData.append('labels', JSON.stringify(labels));
 
     try {
-        const response = await axios.post('https://api.elevenlabs.io/v1/voices/add', formData, {
+        // Upload to ElevenLabs API
+        const elevenLabsResponse = await axios.post('https://api.elevenlabs.io/v1/voices/add', formData, {
             headers: {
                 'accept': 'application/json',
                 'xi-api-key': apiKey,
                 ...formData.getHeaders()
             }
         });
-        res.json(response.data);
+
+        // Update Adalo API after successful upload to ElevenLabs
+        await axios.put(`https://api.adalo.com/v0/apps/9576576a-696d-4132-bdf0-b51ebeae7c34/collections/t_060c36988b7b4f1b935be317a88d0e79/${ElementId}`, {
+            response: "Audio uploaded successfully!"
+        }, {
+            headers: {
+                'Authorization': 'Bearer 93cs2pkvgeemz9dz6haed6aqk',
+                'Content-Type': 'application/json'
+            }
+        });
+
+        res.json(elevenLabsResponse.data);
+
     } catch (error) {
-        res.status(500).send('Error uploading files to ElevenLabs API.');
+        // If there's an error, update Adalo API with the error information
+        try {
+            await axios.put(`https://api.adalo.com/v0/apps/9576576a-696d-4132-bdf0-b51ebeae7c34/collections/t_060c36988b7b4f1b935be317a88d0e79/${ElementId}`, {
+                log: `Error: ${error.message || error}`
+            }, {
+                headers: {
+                    'Authorization': 'Bearer 93cs2pkvgeemz9dz6haed6aqk',
+                    'Content-Type': 'application/json'
+                }
+            });
+        } catch (adaloError) {
+            console.error("Error updating Adalo API:", adaloError.message || adaloError);
+        }
+
+        res.status(500).send('Error processing the request.');
     }
 });
 
